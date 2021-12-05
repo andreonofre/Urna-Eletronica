987a09d32424ac93cdd378ecd7cc2a6ad41a338b
André Onofre <alordandre@gmail.com>
2021-12-05 01:59:05 -0300

    urna

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: "38111",
+                nome: "Fulano de Tal", 
+                partido:"ABC",
+                fotos:[
+                    {url:"vereador1.jpg", legenda: "Vereador"}
+                ]
+            },
+            {
+                numero: "77222",
+                nome: "Beltrano da Silva",
+                partido: "DEFG",
+                fotos:[
+                    {url:"vereador2.jpg", legenda: "Vereador"}
+                ]
+            },   
+        ]
+    }, 
+    {
+        titulo: "PREFEITO",
+        numeros: 2,
+        candidatos:[
+            {
+                numero: "99",
+                nome: "Ciclano",
+                partido: "ABC",
+                vice: "Cic",
+                fotos: [
+                    {url: "prefeito1.jpg", legenda:"Prefeito"},
+                    {url: "viceprefeito1.png", legenda:"Vice-Prefeito", small:true}
+
+                ]
+            },
+            {
+                numero: "84",
+                nome: "Zulano",
+                partido: "Cadeira",
+                vice: "Zul",
+                fotos: [
+                    {url: "prefeita2.jpg", legenda:"Prefeito"},
+                    {url: "viceprefeito2.png", legenda:"Vice-Prefeito", small:true}
+                ]
+            },
+        ]
+    }
+];
